1f3f13b2db1008e7601304d92ac2b7d862f8dc6f
Samruddhikhot <samruddhiShrirangkhot@gmail.com>
2025-07-31 23:28:44 +0530

    added db

--- ImageGalleryBackend/src/main/java/com/controller/ImageController.java
+++ ImageGalleryBackend/src/main/java/com/controller/ImageController.java
@@ -29,10 +29,7 @@ public class ImageController {
 	@Autowired
 	private ImageModelService imageService;
 	
-	@GetMapping("/health")
-public ResponseEntity<String> healthCheck() {
-    return ResponseEntity.ok("OK");
-}
+
 
 	@PostMapping("/upload")
 	public ResponseEntity<String> addFile(
